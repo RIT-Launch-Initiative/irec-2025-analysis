28fd0f4050545e575280ce554808cd9273b78795
Kyle <kms3218@rit.edu>
2025-01-25 15:12:29 -0500

    removed extra tube
    updates masses

--- scripts/test.m
+++ scripts/test.m
@@ -16,33 +16,40 @@ opts = sim.getOptions();
 %set config
 nSims = 10;
 figure;
+turbIntesity = 1;
 opts.setWindTurbulenceIntensity(15)
+opts.setWindSpeedAverage(6.7); 
+opts.setLaunchTemperature(25 + 273.15);  % 25°C
+stabOffRod = zeros(nSims,2);
 
-for I = 1:nSims
-    turbIntesity = 
-    opts.setWindSpeedAverage(15);        % For demonstration, 0 m/s
-    opts.setLaunchTemperature(25 + 273.15);  % 25°C
-    opts.setWindTurbulenceIntensity(15)
-      
+
+for I = 1:nSims      
     % Run single simulation
     iSim = openrocket.simulate(sim, outputs="ALL");
     
     % Retrieve simulation data
     data = openrocket.get_data(sim);
-
+    curStability = data{126, "Stability margin"};
+    stabOffRod (I,1) = curStability;
+    curWindSpeed = data{126,"Wind velocity"};
+    stabOffRod (I,2) = curWindSpeed;
     
-    plot3(data.("Position East of launch")*3.28084, ...
-      data.("Position North of launch")*3.28084, ...
-      data.Altitude*3.28084, 'LineWidth', 1.5);
-    hold on;
-    grid on;
+    % 
+    % plot3(data.("Position East of launch")*3.28084, ...
+    %   data.("Position North of launch")*3.28084, ...
+    %   data.Altitude*3.28084, 'LineWidth', 1.5);
+    % hold on;
+    % grid on;
 end
 
-view(3);
-xlabel('Position East (ft)');
-ylabel('Position North (ft)');
-zlabel('Altitude (ft)');
-title('3D Trajectory (15mph,10%turb))');
+% view(3);
+% xlabel('Position East (ft)');
+% ylabel('Position North (ft)');
+% zlabel('Altitude (ft)');
+% title('3D Trajectory (15mph,10%turb))');
+
+figure
+histogram(stabOffRod)
 
 
 
