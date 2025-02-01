c0b5254e8290af429f9b304ae23a1e3429cbb835
Kyle <kms3218@rit.edu>
2025-01-31 19:18:40 -0500

    Update main_script.m

--- scripts/main_script.m
+++ scripts/main_script.m
@@ -22,32 +22,32 @@ sTomS = 1/1000;
 
 tiledlayout("vertical")
 
-%%
-% STABILITY OF THE ROD HISTOGRAM
-% Purpose: This subscript will show you a histogram of the wind speed you put
-% in. This will tell you the most likely stability off the rod at speed
-% with a confidence interval. Use this to ensure go/no go wind speed is stable.
-
-[stabOffRod,windOffRod] = stabLoop (simName,nSims,turbIntensity,windSpeed,otis_path,temp);
-nexttile 
-hist(stabOffRod,(nSims/10));
-h = findobj(gca,'Type','patch');
-h.FaceColor = ['#F76902'];
-h.EdgeColor = 'w';
-axis padded;
-ylabel('# of events');% ylabl function
-xlabel('Stability (body calibers)'); %xlabel function
-xline(1.5, 'b--', 'Minimum stability required by DTEG', 'LabelVerticalAlignment','middle', 'LabelHorizontalAlignment','center');
-
-title('Standard Distribution of Stability Off the Rod at 15mph'); % Corrected: use title function
-
-%confidence interval for stability. this will give you a 95% CI for
-%your stab off the rod
-x = mean(stabOffRod);
-z = 1.96;
-s = std(stabOffRod);    
-ci_high = x + z*(s/(sqrt(nSims)));
-ci_low  = x - z*(s/(sqrt(nSims)));
+% %%
+% % STABILITY OF THE ROD HISTOGRAM
+% % Purpose: This subscript will show you a histogram of the wind speed you put
+% % in. This will tell you the most likely stability off the rod at speed
+% % with a confidence interval. Use this to ensure go/no go wind speed is stable.
+% 
+% [stabOffRod,windOffRod] = stabLoop (simName,nSims,turbIntensity,windSpeed,otis_path,temp);
+% nexttile 
+% hist(stabOffRod,(nSims/10));
+% h = findobj(gca,'Type','patch');
+% h.FaceColor = ['#F76902'];
+% h.EdgeColor = 'w';
+% axis padded;
+% ylabel('# of events');% ylabl function
+% xlabel('Stability (body calibers)'); %xlabel function
+% xline(1.5, 'b--', 'Minimum stability required by DTEG', 'LabelVerticalAlignment','middle', 'LabelHorizontalAlignment','center');
+% 
+% title('Standard Distribution of Stability Off the Rod at 15mph'); % Corrected: use title function
+% 
+% %confidence interval for stability. this will give you a 95% CI for
+% %your stab off the rod
+% x = mean(stabOffRod);
+% z = 1.96;
+% s = std(stabOffRod);    
+% ci_high = x + z*(s/(sqrt(nSims)));
+% ci_low  = x - z*(s/(sqrt(nSims)));
 
 
 % %% wip 
@@ -82,7 +82,7 @@ ci_low  = x - z*(s/(sqrt(nSims)));
 % %%
 % %temperatue vs apogee 
 % %configure
-% windSpeeds =(0:0.5:10);
+% windSpeeds =(0:0.25:11.5);
 % 
 % %run the sub script
 % [stabWindData,timeStabWindData] = stabWindTemp (windSpeeds,otis_path,simName);
@@ -131,72 +131,71 @@ ci_low  = x - z*(s/(sqrt(nSims)));
 % grid on;
 % grid minor;
 % axis padded;
-% 
-% 
-% 
-% 
-% 
-% %% apogee vs. launch angle
-% %configure
-% high_angle = 86;
-% low_angle = 82;
-% angleStep = 0.1;
-% 
-% %run the sub script
-% [launchAngles,maxAltitudes] = angleApogee (angleStep,high_angle,low_angle,simName,turbIntensity,windSpeed,otis_path);
-% 
-% launchAngles = (low_angle:angleStep:high_angle);
-% 
-% % visulization 
-% nexttile
-% 
-% % Visualization
-% plot(((launchAngles)), maxAltitudes, 'o-', 'MarkerFaceColor', 'b', 'LineWidth', 2);
-% xlabel('Launch Angle (°) from vertical');
-% ylabel('Maximum Altitude (ft)');
-% title(sprintf('Maximum Altitude vs Launch Angle (Wind Speed = 15 mph)'));
-% grid on;
-% box on;
-% axis padded;
-% ylim([8750 11250])
-% yline(11000, 'r--', '+10%', 'LabelVerticalAlignment','middle', 'LabelHorizontalAlignment','center');
-% yline(10000, 'g--', 'Target Apogee', 'LabelVerticalAlignment','middle', 'LabelHorizontalAlignment','center');
-% yline(9000, 'r--', '-10%', 'LabelVerticalAlignment','middle', 'LabelHorizontalAlignment','center');
-% fontsize(16,"points");
-% 
-% %Remove scientific notation from y-axis
-% ax = gca;
-% ax.YAxis.Exponent = 0;
-% ytickformat('%.0f');  % Format y-ticks as whole numbers
-% 
-% %% apogee vs. temperature
-% %configure
-% temperatures = 10:1:50;
-% 
-% [maxAltitudes] = apogeeTemp(temperatures,simName,turbIntensity,windSpeed,otis_path);
-% 
-% maxAltitudes = maxAltitudes*3.28084;
-% 
-% % Visualization
-% nexttile
-% plot(temperatures, maxAltitudes, 'o-', 'MarkerFaceColor', 'b', 'LineWidth', 2);
-% xlabel('Temperature (°C)');
-% ylabel('Maximum Altitude (ft)');
-% axis padded;
-% title(sprintf('Maximum Altitude vs Launch Temperature (Wind Speed = 15 mph)'));
-% grid on;
-% box on;
-% hold on;
-% ylim([8750 11250])
-% yline(11000, 'r--', '+10%', 'LabelVerticalAlignment','middle', 'LabelHorizontalAlignment','center');
-% yline(10000, 'g--', 'Target Apogee', 'LabelVerticalAlignment','middle', 'LabelHorizontalAlignment','center');
-% yline(9000, 'r--', '-10%', 'LabelVerticalAlignment','middle', 'LabelHorizontalAlignment','center');
-% 
-% fontsize(16,"points")
-% 
-% % Remove scientific notation from y-axis
-% ax = gca;
-% ax.YAxis.Exponent = 0;
-% ytickformat('%.0f');  % Format y-ticks as whole numbers
-% 
+
+
+
+
+%% apogee vs. launch angle
+%configure
+high_angle = 86;
+low_angle = 82;
+angleStep = 0.1;
+
+%run the sub script
+[launchAngles,maxAltitudes] = angleApogee (angleStep,high_angle,low_angle,simName,turbIntensity,windSpeed,otis_path);
+
+launchAngles = (low_angle:angleStep:high_angle);
+
+% visulization 
+nexttile
+
+% Visualization
+plot(((launchAngles)), maxAltitudes, 'o-', 'MarkerFaceColor', 'b', 'LineWidth', 2);
+xlabel('Launch Angle (°) from vertical');
+ylabel('Maximum Altitude (ft)');
+title(sprintf('Maximum Altitude vs Launch Angle (Wind Speed = 15 mph)'));
+grid on;
+box on;
+axis padded;
+ylim([8750 11250])
+yline(11000, 'r--', '+10%', 'LabelVerticalAlignment','middle', 'LabelHorizontalAlignment','center');
+yline(10000, 'g--', 'Target Apogee', 'LabelVerticalAlignment','middle', 'LabelHorizontalAlignment','center');
+yline(9000, 'r--', '-10%', 'LabelVerticalAlignment','middle', 'LabelHorizontalAlignment','center');
+fontsize(16,"points");
+
+%Remove scientific notation from y-axis
+ax = gca;
+ax.YAxis.Exponent = 0;
+ytickformat('%.0f');  % Format y-ticks as whole numbers
+
+%% apogee vs. temperature
+%configure
+temperatures = 27:1:46;
+
+[maxAltitudes] = apogeeTemp(temperatures,simName,turbIntensity,windSpeed,otis_path);
+
+maxAltitudes = maxAltitudes*3.28084;
+
+% Visualization
+nexttile
+plot(temperatures, maxAltitudes, 'o-', 'MarkerFaceColor', 'b', 'LineWidth', 2);
+xlabel('Temperature (°C)');
+ylabel('Maximum Altitude (ft)');
+axis padded;
+title(sprintf('Maximum Altitude vs Launch Temperature (Wind Speed = 15 mph)'));
+grid on;
+box on;
+hold on;
+ylim([8750 11250])
+yline(11000, 'r--', '+10%', 'LabelVerticalAlignment','middle', 'LabelHorizontalAlignment','center');
+yline(10000, 'g--', 'Target Apogee', 'LabelVerticalAlignment','middle', 'LabelHorizontalAlignment','center');
+yline(9000, 'r--', '-10%', 'LabelVerticalAlignment','middle', 'LabelHorizontalAlignment','center');
+
+fontsize(16,"points")
+
+% Remove scientific notation from y-axis
+ax = gca;
+ax.YAxis.Exponent = 0;
+ytickformat('%.0f');  % Format y-ticks as whole numbers
+
 % hold off;
