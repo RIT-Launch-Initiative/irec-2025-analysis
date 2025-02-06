0bf832811e2514c8e2d3515fdf35758d01ce1bef
Kyle <kms3218@rit.edu>
2025-02-06 09:53:31 -0500

    working on monte carlo

--- scripts/main/main_script.m
+++ scripts/main/main_script.m
@@ -4,34 +4,180 @@ clear; close all;
 
 %% Global variables
 addpath(genpath("C:\\lmatlib"))
+addpath(genpath("C:\lmatlib\sim"));
 
-
-simName = "15MPH-TEXAS-36C-(TYP)"; %from openrocket
+% open rocket integration intitialize 
 otis_path = "C:\\irec-2025-analysis\\IREC_2025_M6000ST-0.ork";
+otis = openrocket(otis_path);
+sim = otis.sims("15MPH-TEXAS-36C-(TYP)"); %from openrocket
 if ~isfile(otis_path)
     error("No document '%s' found. Ensure the path is correct.", otis_path);
 end
 
-nSims = 1000; % change this to increase number of iterations. higher is better. minimum for any design review is 100 
-turb_spread = 5;
-wind_speed_spread = 10; %m/s
-temp_spread = 20;
-temp = 27; % c
-wind_direction_spread = 45;
+% open rocket integration config
+opts = sim.getOptions();
+
+%Monte carlo variables
+nSims = 15; % change this to increase number of iterations. higher is better. minimum for any design review is 100 
+turb_spread = 0.05;
+wind_speed = 6.7; %m/s
+wind_speed_spread = 5; % m/s
+wind_direction = 45;
+temp_spread = 20; % c
+temp = opts.getLaunchTemperature;
+wind_angle_spread = 45;
 
 %conversion factors
 m_sTOmph = 2.237136;
+meterTofoot = 3.28084;
 sTomS = 1/1000;
 
-data_out = cell(1,nSims)
-for i = 1:length(data_out)
-    opts.setWindDirection(-opts.getLaunchRodDirection + ...
-        deg2rad((rand()-0.5)*wind_angle_spread));
+%initialize 
+data_stabilityOffRod = zeros(1, nSims);
+data_wind_speeds = zeros(1,nSims);
+data_temp = zeros(1,nSims);
+data_wind_direciton = zeros(1,nSims);
+data_apogee = zeros(1,nSims);
+data_time_to_stab = zeros(1,nSims);
+
+
+
+
+for I = 1:nSims
+    % set randomized sim variable
+    wind_dir = wind_direction+(rand()-0.5)*wind_angle_spread;
+    opts.setWindDirection(wind_dir);    
     opts.setWindSpeedAverage(wind_speed + (rand()-0.5)*wind_speed_spread);
     opts.setLaunchTemperature(temp + (rand()-0.5)*temp_spread);
 
-    data_out{i} = openrocket.simulate(sim, outputs = "ALL");
+    % run simulation
+    data = openrocket.simulate(sim, outputs = "ALL");
+
+    data_apogee(1,I) = max(data.Altitude);
+
+
+    %limit data range
+    data_range = timerange(eventfilter("LAUNCHROD"), eventfilter("BURNOUT"), "openleft");
+    data = data(data_range, :);
+            
+    % collect interesting information
+    stabilityMargin = data{:, 'Stability margin'};
+    data_stabilityOffRod (1,I) = data{1, 'Stability margin'};
+
+    if data_stabilityOffRod(1,I) >= 1.5
+        data_time_to_stab(1,I) = 0;
+    else
+        % Define the subarray starting at index 127
+        marginSubarray = stabilityMargin(1:end);
+        timeSubarray = seconds(data.Time(1:end));
+
+        if any(marginSubarray >= 1.5)% Find the index range where stabilityMargin crosses 1.5
+            % Use interp1 to find the exact time where stabilityMargin reaches 1.5
+            data_time_to_stab(1,I) = (interp1(marginSubarray, timeSubarray, 1.5))-(timeSubarray(1,1));
+        else
+            data_time_to_stab(1,I) = NaN;  % No stability achieved
+        end    
+    end
+
+
+
+    data_wind_speeds(1,I) = data{1,"Wind velocity"};
+    data_temp(1,I) = data{1,"Air temperature"};
+    data_wind_direciton(1,I) = wind_dir;
+
+    opts.randomizeSeed    
+    disp(I)
+
 end
+
+%convert MKS to mph,C
+data_apogee = data_apogee*meterTofoot;
+data_temp = data_temp-273.15; % K to C
+
+figure;
+tiledlayout(1,3)
+
+% First tile: Wind Speeds vs. Stability Off Rod
+nexttile;
+scatter(data_wind_speeds(1,:), data_stabilityOffRod(1,:), "filled")
+xlabel("Wind Speeds")
+ylabel("Stability Off Rod")
+plotlables(2);
+
+
+% Second tile: Wind Direction vs. Stability Off Rod
+nexttile;
+scatter(data_wind_direciton(1,:), data_stabilityOffRod(1,:), "filled")
+xlabel("Wind Direction")
+ylabel("Stability Off Rod")
+plotlables(2);
+
+% Third tile: Temperature vs. Stability Off Rod
+nexttile;
+for J = J:height(data_time_to_stab)
+    if data_stabilityOffRod(1,J) <= 1.5
+        scatter(data_stabilityOffRod(1,:), data_time_to_stab(1,:), "filled")
+        xlabel("Stability Off Rod")
+        ylabel("Time to 1.5 cal")
+        xlim([1 2])
+        xlabel('Stability (body calibers)'); %xlabel function
+        xline(1.5, 'b--', 'Minimum stability required by DTEG', 'LabelVerticalAlignment','middle', 'LabelHorizontalAlignment','center');
+    end
+end
+
+
+
+
+figure;
+
+tiledlayout(1,3)
+
+
+nexttile;
+scatter(data_wind_speeds(1,:), data_apogee(1,:), "filled")
+xlabel("Wind Speeds")
+ylabel("Apogee")
+ylim([8750 11250])
+ax = gca; % axes handle
+ax.YAxis.Exponent = 0;
+plotlables(1);
+
+nexttile;
+scatter(data_wind_direciton(1,:), data_apogee(1,:), "filled")
+xlabel("Wind Direction")
+ylabel("Apogee")
+ylim([8750 11250])
+ax = gca; % axes handle
+ax.YAxis.Exponent = 0;
+plotlables(1);
+
+nexttile;
+scatter(data_temp(1,:), data_apogee(1,:), "filled")
+xlabel("Temperature")
+ylabel("Apogee")
+ylim([8750 11250])
+ax = gca; % axes handle
+ax.YAxis.Exponent = 0;
+plotlables(1);
+
+function plotlables(config)
+    if config == 1
+        yline(11000, 'r--', '+10%', 'LabelVerticalAlignment','middle', 'LabelHorizontalAlignment','center');
+        yline(10000, 'g--', 'Target Apogee', 'LabelVerticalAlignment','middle', 'LabelHorizontalAlignment','center');
+        yline(9000, 'r--', '-10%', 'LabelVerticalAlignment','middle', 'LabelHorizontalAlignment','center');
+        fontsize(16,"points");
+    elseif config == 2
+        ylim([1 2])
+        ylabel('Stability (body calibers)'); %xlabel function
+        yline(1.5, 'b--', 'Minimum stability required by DTEG', 'LabelVerticalAlignment','middle', 'LabelHorizontalAlignment','center');
+
+    end
+
+end
+
+
+
+
 
 
 
@@ -43,26 +189,26 @@ end
 % in. This will tell you the most likely stability off the rod at speed
 % with a confidence interval. Use this to ensure go/no go wind speed is stable.
 
-[stabOffRod,windOffRod] = stabLoop (simName,nSims,turbIntensity,windSpeed,otis_path,temp);
-nexttile 
-hist(stabOffRod,(nSims/10));
-h = findobj(gca,'Type','patch');
-h.FaceColor = ['#F76902'];
-h.EdgeColor = 'w';
-axis padded;
-ylabel('# of events');% ylabl function
-xlabel('Stability (body calibers)'); %xlabel function
-xline(1.5, 'b--', 'Minimum stability required by DTEG', 'LabelVerticalAlignment','middle', 'LabelHorizontalAlignment','center');
-
-title('Standard Distribution of Stability Off the Rod at 15mph'); % Corrected: use title function
+% [stabOffRod,windOffRod] = stabLoop (simName,nSims,turbIntensity,windSpeed,otis_path,default_temp);
+% nexttile 
+% hist(stabOffRod,(nSims/10));
+% h = findobj(gca,'Type','patch');
+% h.FaceColor = ['#F76902'];
+% h.EdgeColor = 'w';
+% axis padded;
+% ylabel('# of events');% ylabl function
+% xlabel('Stability (body calibers)'); %xlabel function
+% xline(1.5, 'b--', 'Minimum stability required by DTEG', 'LabelVerticalAlignment','middle', 'LabelHorizontalAlignment','center');
+% 
+% title('Standard Distribution of Stability Off the Rod at 15mph'); % Corrected: use title function
 
 %confidence interval for stability. this will give you a 95% CI for
 %your stab off the rod
-x = mean(stabOffRod);
-z = 1.96;
-s = std(stabOffRod);    
-ci_high = x + z*(s/(sqrt(nSims)));
-ci_low  = x - z*(s/(sqrt(nSims)));
+% x = mean(stabOffRod);
+% z = 1.96;
+% s = std(stabOffRod);    
+% ci_high = x + z*(s/(sqrt(nSims)));
+% ci_low  = x - z*(s/(sqrt(nSims)));
 
 % 
 % % %% wip 
