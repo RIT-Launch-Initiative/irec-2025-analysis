f607f07f3dd9bf398270a8b3e5142f50ef467ec1
Kyle <kms3218@rit.edu>
2025-06-07 09:31:53 -0400

    added timerange setup

--- scripts/irec/prelaunch.m
+++ scripts/irec/prelaunch.m
@@ -3,20 +3,17 @@
 % goal: pull weather forecasts. estimate optimal nose mass
 clear;
 
+% conversions
+m2f=3.28084;
 
 otis = openrocket("rocket_files\IREC_2025_M6000ST-0.ork");
 sim = otis.sims("10MPH-TEXAS-36C-(TYP)");
 site = launchsites("spaceport-midland");
 
-disp("Available NCEP models:");
-ncep.list
-
-disp("Available GFS output grids: ");
-ncep.list("gfs")
+baseline_flight_data = otis.simulate(sim, outputs = "ALL");
 
 % Atmospheric model
-times = datetime(2025, 06, 08, TimeZone = -hours(6)) + hours([10 12]);
-launchtime = datetime(2024, 06, 08, 10, 21, 00, TimeZone = -hours(6));
+launchtime = datetime(2024, 06, rand, 10, 00, 00, TimeZone = -hours(6));
 
 %% Custom atmosphere & wind models
 % Get air data table
@@ -25,3 +22,19 @@ airdata = atmosphere("gfs", "pgrb2.1p00", site.lat, site.lon, launchtime, ...
 
 % Celcius to Kelvin
 airdata.TMP = airdata.TMP + 273.15;
+
+data = otis.simulate(sim, outputs = "ALL", ...
+    wind = airdata(:, ["HGT", "UGRD", "VGRD", "PRES", "TMP"]));
+
+apogee = max(data.Altitude) * m2f
+
+fprintf("Maximum altituide %0.0f feet\n",apogee)
+
+figure;
+
+plot_openrocket(data, "Altitude", "Total velocity", ...
+    end_ev = "GROUND_HIT", labels = ["BURNOUT", "APOGEE", "MAIN"]);
+
+figure
+plot_openrocket(data, "Stability margin", "Angle of attack", ...
+    start_ev = "LAUNCHROD", end_ev = "APOGEE", labels = ["LAUNCHROD", "BURNOUT"]);
